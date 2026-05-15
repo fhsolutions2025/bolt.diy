63057e122b7280b8a6cb2385a22b3a1ecd1ea943
fhsolutions2025 <teams@fullhousey.com>
2026-05-15 16:34:14 +0530

    Update entry.client.tsx

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,21 @@
 import { RemixBrowser } from '@remix-run/react';
-import { startTransition } from 'react';
+import { startTransition, StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
-startTransition(() => {
-  hydrateRoot(document.getElementById('root')!, <RemixBrowser />);
-});
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>,
+    );
+  });
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  // Safari and older browsers
+  window.setTimeout(hydrate, 1);
+}
